a9a8ef2c143ba3c46d8b5c417224058e5aee514f
Luke-Aaron-Hoffman <35440461+Luke-Aaron-Hoffman@users.noreply.github.com>
2019-08-11 23:08:40 -0400

    Suit Display
    Added code to check the config for "displaySuitAsSymbol," which as the name suggests will decide to use either symbols or letters to display card suits. This is mostly in case Suits don't display correctly.

--- GameOfWar/Properties/Settings.Designer.cs
+++ GameOfWar/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace GameOfWar.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -56,6 +56,15 @@ namespace GameOfWar.Properties {
         public int aceHigh {
             get {
                 return ((int)(this["aceHigh"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool displaySuitAsSymbol {
+            get {
+                return ((bool)(this["displaySuitAsSymbol"]));
             }
         }
     }
